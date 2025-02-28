4e39785c262d44cd95d9296f59887900afee3d58
ericssendelima <ericssendelima@gmail.com>
2025-02-28 14:47:32 -0300

    Correção do script do package.json

--- src/router.js
+++ src/router.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
+import path from "path";
 
 //Controllers
 import { NewUserController } from "./controllers/NewUserController.js";
 import { UsersListController } from "./controllers/UsersListController.js";
-import { HomepageServerController } from "./controllers/HomePageServerController.js";
 import { NewExerciseController } from "./controllers/NewExerciseController.js";
 import { LogsListController } from "./controllers/LogsListController.js";
 import { testeController } from "./controllers/testeController.js";
@@ -15,16 +15,27 @@ import { databaseUsers } from "./middlewares/databaseUsers.js";
 const routes = Router();
 
 //Requisições GET
-routes.get("/", new HomepageServerController().handle); //Homepage
+routes.get("/", (req, res) => {
+  const __dirname = path.resolve();
+  return res.sendFile(__dirname + "/views/index.html");
+}); //Homepage
+
 routes.get("/api/users", databaseUsers, new UsersListController().handle); //Lista de usuários
-routes.get("/api/users/:_id/logs", databaseUsers, new LogsListController().handler); //Logs de um usuário especifico
+routes.get(
+  "/api/users/:_id/logs",
+  databaseUsers,
+  new LogsListController().handler
+); //Logs de um usuário especifico
 routes.get("/limpar-dados", new LimparDados().handle); //Limpa todos os dados
 // routes.get("/teste/:_id", new testeController().handle); //Logs de um usuário especifico
 routes.get("/teste", databaseUsers, new testeController().handle); //Logs de um usuário especifico
 
-
 //Requisições POST
-routes.post("/api/users", databaseUsers,new NewUserController().handle); //cria um novo usuário
-routes.post("/api/users/:_id/exercises", databaseUsers, new NewExerciseController().handle); //cria um novo exercício de um novo usuário
+routes.post("/api/users", databaseUsers, new NewUserController().handle); //cria um novo usuário
+routes.post(
+  "/api/users/:_id/exercises",
+  databaseUsers,
+  new NewExerciseController().handle
+); //cria um novo exercício de um novo usuário
 
 export { routes };
